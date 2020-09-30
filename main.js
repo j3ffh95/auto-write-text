1f2f22ee7f76b6a003c14b247325ebbea7bc3d20
j3ffh95 <j3ffh95@gmail.com>
2020-09-30 09:09:03 -0400

    Finished with JS logic

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const text =
   "The mountain may seem very high, It reaches even to the sky, And yet the picture holds it all As well as things quite near and small, Ans then the picture's but a nook In my small picture reading book.";
 
-let index = 0
+let index = 0;
 
-conts writeText = () => {
-  document.body.innerText = text.slice(0, index)
+const writeText = () => {
+  document.body.innerText = text.slice(0, index);
 
   index++;
 
-  if(index > text.length) index = 0;
-}
+  if (index > text.length) {
+    index = 0;
+  }
+};
+
+setInterval(writeText, 100);
